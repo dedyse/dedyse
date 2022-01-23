104635501bb86f8b86a6ce96e0ec77a1574816bf
dedyse <98254243+dedyse@users.noreply.github.com>
2022-01-23 15:11:27 +0700

    Add files via upload

---
+++ adsterra300x250.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '780356d766cf49fb01b683eef7896b66',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/780356d766cf49fb01b683eef7896b66/invoke.js"></scr' + 'ipt>');
+</script>
